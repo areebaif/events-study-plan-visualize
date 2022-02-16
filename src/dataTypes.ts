166b4cc9196697ef47d9444e69202ec4ad79010b
areeba-if <areebaiftikhar1993@gmail.com>
2022-02-16 11:42:02 -0600

    events-updated-to-include-programming-lng-events

--- src/dataTypes.ts
+++ src/dataTypes.ts
@@ -1,21 +1,94 @@
 import { Subjects } from "./subjects";
 
 // This file contains all types of data emited by events
+
+///////////////////
+// All Events Related to Skills Service
 export interface skillCreatedEvent {
-  // TODO: there should be a transaction number
   subject: Subjects.SkillCreated;
   data: {
     _id: string;
     name: string;
     version: number;
+    dbStatus: string;
+  };
+}
+
+export interface skillUpdatedEvent {
+  subject: Subjects.SkillUpdated;
+  data: {
+    _id: string;
+    name: string;
+    version: number;
+    dbStatus: string;
   };
 }
 
 export interface skillDeletedEvent {
-  // TODO: there should be a transaction number
   subject: Subjects.SkillDeleted;
   data: {
     _id: string;
     version: number;
+    dbStatus: string;
+  };
+}
+
+///////////////////
+// All Events Related to Programming Language Service
+export interface programmingLngCreatedEvent {
+  subject: Subjects.ProgrammingLanguageCreated;
+  data: {
+    _id: string;
+    name: string;
+    version: number;
+    dbStatus: string;
+  };
+}
+
+export interface programmingLngUpdatedEvent {
+  subject: Subjects.ProgrammingLanguageUpdated;
+  data: {
+    _id: string;
+    name: string;
+    version: number;
+    dbStatus: string;
+  };
+}
+
+export interface programmingLngDeletedEvent {
+  subject: Subjects.ProgrammingLanguageDeleted;
+  data: {
+    _id: string;
+    version: number;
+    dbStatus: string;
+  };
+}
+
+///////////////////
+// All Events Related to Course Service
+export interface courseCreatedEvent {
+  subject: Subjects.CourseCreated;
+  data: {
+    _id: string;
+    name: string;
+    skill: string[];
+    language: string[];
+  };
+}
+
+export interface courseUpdatedEvent {
+  subject: Subjects.CourseUpdated;
+  data: {
+    _id: string;
+    name: string;
+    skill: string[];
+    language: string[];
+  };
+}
+
+export interface courseDeletedEvent {
+  subject: Subjects.CourseDeleted;
+  data: {
+    _id: string;
   };
 }
--- src/subjects.ts
+++ src/subjects.ts
@@ -1,8 +1,10 @@
 // this file contains all the channel names different services will subscribe to recieve, create event
 export enum Subjects {
   SkillCreated = "skill:created",
+  SkillUpdated = "skill:updated",
   SkillDeleted = "skill:deleted",
   ProgrammingLanguageCreated = "programminglanguage:created",
+  ProgrammingLanguageUpdated = "programminglanguage:updated",
   ProgrammingLanguageDeleted = "programminglanguage:deleted",
   CourseCreated = "course:created",
   CourseUpdated = "course:updated",
