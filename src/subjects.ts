4a22cce4f0da73894c368322f524a11ad15887b3
areeba-if <areebaiftikhar1993@gmail.com>
2022-02-16 00:21:48 -0600

    initial event data

---
+++ src/subjects.ts
@@ -0,0 +1,13 @@
+// this file contains all the channel names different services will subscribe to recieve, create event
+export enum Subjects {
+  SkillCreated = "skill:created",
+  SkillDeleted = "skill:deleted",
+  ProgrammingLanguageCreated = "programminglanguage:created",
+  ProgrammingLanguageDeleted = "programminglanguage:deleted",
+  CourseCreated = "course:created",
+  CourseUpdated = "course:updated",
+  CourseDeleted = "course:deleted",
+  BookCreated = "book:created",
+  BookUpdated = "book:updated",
+  BookDeleted = "book:deleted",
+}
